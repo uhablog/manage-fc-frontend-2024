316e00688918955b60ba9e491e1552347b0ef3a8
uhablog <uha.blog98@gmail.com>
2024-06-07 09:04:39 +0900

    認証機能追加

--- src/app/convention/page.tsx
+++ src/app/convention/page.tsx
@@ -1,22 +1,29 @@
-import { Session, getSession } from "@auth0/nextjs-auth0";
+import { Session, getAccessToken, getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import Image from "next/image";
+import CheckAPI from "../component/CheckAPI";
 
-export default async function ConventionPage() {
+export default withPageAuthRequired( async function ConventionPage() {
   const session: Session | null | undefined = await getSession();
   const user = session?.user;
 
+  if (!user) {
+    return <p>Loading...</p>
+  }
+
+  const access_token = (await getAccessToken()).accessToken;
+
   return (
-    user && (
-      <>
-        {/* <Image
-          src={user.picture}
-          alt={user.name}
-          width={400}
-          height={400}
-        /> */}
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-      </>
-    )
+    <>
+      <Image
+        src={user.picture}
+        alt={user.name}
+        width={400}
+        height={400}
+      />
+      <h2>{user.name}</h2>
+      <p>{user.email}</p>
+      <a href="/api/auth/logout">logout</a><br/>
+      <CheckAPI access_token={access_token} />
+    </>
   )
-};
+}, { returnTo: '/convention'});
