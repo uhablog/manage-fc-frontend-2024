1e2968dc4e5b19a8cfeedcc8a5802d968835d575
uhablog <uha.blog98@gmail.com>
2024-07-05 09:26:27 +0900

    チーム間で移籍できるようにした

---
+++ src/app/api/team/transfer/route.ts
@@ -0,0 +1,21 @@
+import { getAccessToken } from "@auth0/nextjs-auth0";
+
+export async function POST(
+  request: Request
+) {
+  const accessTokenResult = await getAccessToken();
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/team/transfer`, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${accessTokenResult.accessToken}`,
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(await request.json())
+  });
+  const json = await res.json();
+
+  return Response.json({
+    ...json
+  });
+};
